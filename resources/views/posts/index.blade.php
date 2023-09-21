dc01137c7b886d444de090bdfb89a3f49b53d162
Tachizaki <php.portfolio1117@gmail.com>
2023-09-21 22:59:33 +0900

    migrate commit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,10 +12,11 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                        <h2 class='title'>
-                            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                        </h2>
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
                     <p class='body'>{{ $post->body }}</p>
+                    <a href="">{{ $post->category->name }}</a>
                     <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
                         @csrf
                         @method('DELETE')
